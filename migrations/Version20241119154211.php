c556ffbf75c587800ba8f3b20ae16d3de06157d5
florent-inzerty <florent.devynck@groupevitaminet.com>
2024-11-19 17:23:52 +0100

    fix:swagger

--- migrations/Version20241119154211.php
+++ migrations/Version20241119154211.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241118152005 extends AbstractMigration
+final class Version20241119154211 extends AbstractMigration
 {
     public function getDescription(): string
     {
