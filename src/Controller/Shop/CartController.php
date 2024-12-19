913d1ba4b60d949bf4b1114486b7d32724ff9675
franck59840 <depoorter.franck@gmail.com>
2024-12-19 17:43:50 +0100

    create page shop, service data, controller shop and cart

---
+++ src/Controller/Shop/CartController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Cart;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    #[Route('/panier', name: 'cart_index')]
+    public function index(): Response
+        {
+        
+       
+
+        return $this->render('cart/cart.html.twig');
+
+    }
+}
