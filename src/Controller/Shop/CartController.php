2bf85bca04a66fdc6540050982fb412306bd3e05
franck59840 <depoorter.franck@gmail.com>
2024-12-20 14:44:11 +0100

    vue shop,componant navbar + modif base vue Add var color

--- src/Controller/Shop/CartController.php
+++ src/Controller/Shop/CartController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Cart;
+namespace App\Controller\Shop;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
