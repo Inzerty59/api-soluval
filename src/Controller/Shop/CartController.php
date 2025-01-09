8586ea438efb33bfbaff5efca588443a9d23a6a9
franck59840 <depoorter.franck@gmail.com>
2025-01-09 15:31:19 +0100

    add shop , cart iin progress

--- src/Controller/Shop/CartController.php
+++ src/Controller/Shop/CartController.php
@@ -3,18 +3,63 @@
 namespace App\Controller\Shop;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\DataImporter;
 
 class CartController extends AbstractController
 {
     #[Route('/panier', name: 'cart_index')]
-    public function index(): Response
-        {
+    public function index(SessionInterface $session, DataImporter $dataImporter): Response
+    {
+        $cart = $session->get('cart', []);
+        $allProducts = $dataImporter->fetchData()['products'];
+        $cartWithDetails = [];
+
+        foreach ($cart as $id => $quantity) {
+            foreach ($allProducts as $product) {
+                if ($product['part']['Id'] === $id) {
+                    $cartWithDetails[] = [
+                        'product' => $product['part'],
+                        'quantity' => $quantity,
+                    ];
+                    break;
+                }
+            }
+        }
+
+        return $this->render('cart/cart.html.twig', [
+            'cart' => $cartWithDetails,
+        ]);
+    }
+
+    #[Route('/ajouter-au-panier/{id}', name: 'cart_add')]
+    public function addToCart($id, SessionInterface $session, Request $request): Response
+    {
+        $cart = $session->get('cart', []);
         
-       
 
-        return $this->render('cart/cart.html.twig');
+        $cart[$id] = ($cart[$id] ?? 0) + 1;
 
+        $session->set('cart', $cart);
+
+        $referer = $request->headers->get('referer');
+        return $this->redirect($referer ?: $this->generateUrl('shop_index'));
+    }
+
+    #[Route('/retirer-du-panier/{id}', name: 'cart_remove')]
+    public function removeFromCart($id, SessionInterface $session): Response
+    {
+        $cart = $session->get('cart', []);
+
+        if (isset($cart[$id])) {
+            unset($cart[$id]);
+        }
+
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('cart_index');
     }
 }
--- src/Service/DataImporter.php
+++ src/Service/DataImporter.php
@@ -5,7 +5,6 @@ class DataImporter
 {
     public function fetchData(): array
     {
-        // Retourne toutes les données nécessaires
         return [
             'products' => [
                 [
@@ -13,7 +12,7 @@ class DataImporter
                         'name' => 'Filtre à huile',
                         'modelName' => 'PEUGEOT 206 PHASE 2',
                         'ManufacturerReference' => '6242C3',
-                        'Id' => '98071900',
+                        'Id' => '1',
                         'Name' => 'Com (Bloc Contacteur Tournant+Commodo Essuie Glace+Commodo Phare) PEUGEOT 206 PHASE 2 Diesel',
                         'Category' => 'Com (Bloc Contacteur Tournant+Commodo Essuie Glace+Commodo Phare)',
                         'Description' => 'Description non renseignée.',
@@ -46,24 +45,6 @@ class DataImporter
                     'vehicle' => [
                         'Mileage' => '265370',
                         'Vignette' => 'https://opisto-prod-pic.opisto.s3.eu-west-1.bso.st/4672/vhu_photo/2024/10/Vehicule-PEUGEOT-206%20PHASE%202-2006-93a221912243fa960f52ec459daa6cbe2914fc0a51c9d58a4fb25a8966fd312a_o.jpg',
-                        'ScaledPhotos' => [
-                            'https://opisto-prod-pic.opisto.s3.eu-west-1.bso.st/4672/vhu_photo/2024/10/Vehicule-PEUGEOT-206%20PHASE%202-2006-78c8663be43a095a2e533dd400e96eefffad89a709f8e005076c1fd948d15c8f_s.jpg',
-                            'https://opisto-prod-pic.opisto.s3.eu-west-1.bso.st/4672/vhu_photo/2024/10/Vehicule-PEUGEOT-206%20PHASE%202-2006-d14af51e98f82fa65efed7e7f4eeb0da69eb55d3500d6838d1f57bba520ab3d3_s.jpg',
-                            'https://opisto-prod-pic.opisto.s3.eu-west-1.bso.st/4672/vhu_photo/2024/10/Vehicule-PEUGEOT-206%20PHASE%202-2006-e0bc877b88f51a810cbb92fd36694ddd69f7aa19f4711286b7c98b955af89929_l.jpg',
-                        ],
-                    ],
-                    'color' => [
-                        'Name' => 'Gris clair',
-                    ],
-                    'partPrice' => [
-                        'OriginPrice' => '90',
-                        'Quantity' => '2',
-                        'VATRate' => '5.5',
-                    ],
-                    'partLight' => [
-                        'CasseId' => '4672',
-                        'Price' => '90',
-                        'Shipping' => '1',
                     ],
                 ],
                 [
@@ -71,7 +52,7 @@ class DataImporter
                         'name' => 'Plaquettes de frein',
                         'modelName' => 'PEUGEOT 208',
                         'ManufacturerReference' => '6242D7',
-                        'Id' => '98102301',
+                        'Id' => '2',
                         'Name' => 'Plaquettes de frein avant pour PEUGEOT 208 Diesel',
                         'Category' => 'Freinage',
                         'Description' => 'Plaquettes de frein compatibles avec PEUGEOT 208.',
@@ -103,25 +84,7 @@ class DataImporter
                     ],
                     'vehicle' => [
                         'Mileage' => '125000',
-                        'Vignette' => 'https://example.com/images/peugeot-208.jpg',
-                        'ScaledPhotos' => [
-                            'https://example.com/images/peugeot-208-1.jpg',
-                            'https://example.com/images/peugeot-208-2.jpg',
-                            'https://example.com/images/peugeot-208-3.jpg',
-                        ],
-                    ],
-                    'color' => [
-                        'Name' => 'Noir métallisé',
-                    ],
-                    'partPrice' => [
-                        'OriginPrice' => '50',
-                        'Quantity' => '5',
-                        'VATRate' => '20',
-                    ],
-                    'partLight' => [
-                        'CasseId' => '4673',
-                        'Price' => '50',
-                        'Shipping' => '1',
+                        'Vignette' => 'https://opisto-prod-pic.opisto.s3.eu-west-1.bso.st/4672/vhu_photo/2024/10/Vehicule-PEUGEOT-206%20PHASE%202-2006-93a221912243fa960f52ec459daa6cbe2914fc0a51c9d58a4fb25a8966fd312a_o.jpg',
                     ],
                 ],
             ],
