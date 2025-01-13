39cc2509f06dfbc421d727eb6580b32f5e226466
florent-inzerty <florent.devynck@groupevitaminet.com>
2025-01-13 18:02:32 +0100

    fix:regex, siret

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,12 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ApiResource(
-    operations: [
-        new \ApiPlatform\Metadata\GetCollection(),
-        new \ApiPlatform\Metadata\Get()
-    ]
-)]#[ORM\Table(name: 'user')]
+#[ORM\Table(name: 'user')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
