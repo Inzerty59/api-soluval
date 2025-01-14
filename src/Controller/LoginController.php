edc146bece528e22a57c4ea36a965c46b4f631b9
florent-inzerty <florent.devynck@groupevitaminet.com>
2025-01-14 11:27:22 +0100

    fix:connexion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,53 +2,23 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\LoginFormType;
-use App\Service\OAuth2LoginService;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     #[Route('/connexion', name: 'app_login')]
-    public function login(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        OAuth2LoginService $loginService,
-        SessionInterface $session
-    ): Response {
-        $form = $this->createForm(LoginFormType::class);
-        $form->handleRequest($request);
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-            $email = $data['email'];
-            $password = $data['password'];
-
-            try {
-                $accessToken = $loginService->login($email, $password);
-
-                $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-                if (!$user) {
-                    throw new \Exception("Utilisateur non trouvé.");
-                }
-
-                $this->addFlash('success', 'Connexion réussie.');
-
-                $session->set('api_token', $accessToken);
-
-                return $this->redirectToRoute('app_home');
-            } catch (\Exception $e) {
-                $this->addFlash('error', $e->getMessage());
-            }
-        }
-
-        return $this->render('login/login.html.twig', [
-            'loginForm' => $form->createView(),
-        ]);
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
++           'error'         => $error,
+]);
     }
 }
