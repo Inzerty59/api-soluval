09362ac12e96b4bbf5c1c1753901482742fb3c2c
florent-inzerty <florent.devynck@groupevitaminet.com>
2025-01-14 17:57:05 +0100

    wip:form-ui

---
+++ assets/js/passwordVisibilityRegistration.js
@@ -0,0 +1,16 @@
+function togglePasswordVisibility(passwordFieldId, eyeIconId) {
+    const passwordField = document.getElementById(passwordFieldId);
+    const eyeIcon = document.getElementById(eyeIconId);
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        eyeIcon.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7 1.274-4.057 5.064-7 9.542-7 1.478 0 2.875.325 4.125.825M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M4.5 4.5l15 15" />';
+    } else {
+        passwordField.type = 'password';
+        eyeIcon.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M2.458 12C3.732 7.943 7.522 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.478 0-8.268-2.943-9.542-7z" />';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('eye-icon1').addEventListener('click', () => togglePasswordVisibility('password', 'eye-icon1'));
+    document.getElementById('eye-icon2').addEventListener('click', () => togglePasswordVisibility('confirm-password', 'eye-icon2'));
+});
