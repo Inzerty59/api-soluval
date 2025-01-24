d8983e3ee85fb4f6c3486fc1a903f8033617062f
florent-inzerty <florent.devynck@groupevitaminet.com>
2025-01-24 10:49:20 +0100

    fix:paiement-display

--- src/Controller/Shop/OrderSummaryController.php
+++ src/Controller/Shop/OrderSummaryController.php
@@ -4,15 +4,23 @@ namespace App\Controller\Shop;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderSummaryController extends AbstractController
 {
     #[Route('/recapulatif', name: 'orderSummary_page')]
-    public function paymentPage(): Response
+    public function paymentPage(SessionInterface $session): Response
     {
+        $cart = $session->get('cart', []);
+
+        $total = array_reduce($cart, function ($carry, $item) {
+            return $carry + ($item['part']->getFinalPrice() * ($item['quantity'] ?? 1));
+        }, 0);
+
         return $this->render('payment/orderSummary.html.twig', [
-            'controller_name' => 'OrdersummaryController',
-    ]);
+            'cart' => $cart,
+            'total' => $total,
+        ]);
     }
 }
