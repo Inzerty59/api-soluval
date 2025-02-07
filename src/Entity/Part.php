6a2fb90d320dc1f4c3af6b899e0e7895febd6a43
florent-inzerty <florent.devynck@groupevitaminet.com>
2025-02-07 18:57:54 +0100

    :wip:to-send/free-shipping

--- src/Entity/Part.php
+++ src/Entity/Part.php
@@ -105,18 +105,14 @@ use Doctrine\ORM\Mapping as ORM;
     #[ORM\Column]
     private ?int $Shipping_id = null;
 
-    /**
-     * @var Collection<int, Order>
-     */
-    #[ORM\ManyToMany(targetEntity: Order::class, inversedBy: 'parts')]
-    private Collection $category;
+    #[ORM\ManyToOne(inversedBy: 'parts')]
+    private ?Order $order = null;
 
     #[ORM\Column(length: 255)]
     private ?string $weight = null;
 
     public function __construct()
     {
-        $this->category = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -497,30 +493,14 @@ use Doctrine\ORM\Mapping as ORM;
         return $this;
     }
 
-    public function getCategory(): ?Order
+    public function getOrder(): ?Order
     {
-        return $this->category;
+        return $this->order;
     }
 
-    public function setCategory(?Order $category): static
+    public function setOrder(?Order $order): static
     {
-        $this->category = $category;
-
-        return $this;
-    }
-
-    public function addCategory(Order $category): static
-    {
-        if (!$this->category->contains($category)) {
-            $this->category->add($category);
-        }
-
-        return $this;
-    }
-
-    public function removeCategory(Order $category): static
-    {
-        $this->category->removeElement($category);
+        $this->order = $order;
 
         return $this;
     }
