05e43b638aab22b597633ff3b851763cbd890c3c
florent-inzerty <florent.devynck@groupevitaminet.com>
2025-02-18 10:51:19 +0100

    feat:mail password forgotten

---
+++ src/Controller/PasswordResetController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class PasswordResetController extends AbstractController
+{
+    #[Route('/reinitialisation', name: 'app_forgot_password')]
+    public function request(Request $request, EntityManagerInterface $entityManager, TokenGeneratorInterface $tokenGenerator): Response
+    {
+        $emailNotFound = false;
+
+        if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+            $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+            if ($user) {
+                $token = $tokenGenerator->generateToken();
+                $user->setResetToken($token);
+                $entityManager->flush();
+
+                $resetUrl = $this->generateUrl('app_reset_password', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+
+                $dsn = $_ENV['MAILER_DSN'];
+                $transport = Transport::fromDsn($dsn);
+                $mailer = new Mailer($transport);
+
+                $email = (new Email())
+                    ->from('florent.devynck@groupevitaminet.com')
+                    ->to($user->getEmail())
+                    ->subject('Réinitialisation de votre mot de passe')
+                    ->text('Pour réinitialiser votre mot de passe, veuillez cliquer sur le lien suivant : ' . $resetUrl);
+
+                $mailer->send($email);
+
+                $this->addFlash('success', 'Un e-mail de réinitialisation de mot de passe a été envoyé.');
+
+                return $this->redirectToRoute('app_login');
+            }
+
+            $emailNotFound = true;
+            $this->addFlash('error', 'Cette adresse e-mail n\'existe pas.');
+        }
+
+        return $this->render('security/forgot_password.html.twig', ['emailNotFound' => $emailNotFound]);
+    }
+
+    #[Route('/reinitialisation/{token}', name: 'app_reset_password')]
+    public function reset(Request $request, string $token, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = $entityManager->getRepository(User::class)->findOneBy(['resetToken' => $token]);
+
+        if (!$user || $user->getResetToken() === null) {
+            $this->addFlash('error', 'Lien invalide ou déjà utilisé.');
+            return $this->redirectToRoute('shop_index');
+        }
+
+        if ($request->isMethod('POST')) {
+            $newPassword = $request->request->get('password');
+            $confirmPassword = $request->request->get('confirm_password');
+
+            if ($newPassword !== $confirmPassword) {
+                $this->addFlash('error', 'Les mots de passe ne correspondent pas.');
+                return $this->render('security/reset_password.html.twig', ['token' => $token]);
+            }
+
+            if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/', $newPassword)) {
+                $this->addFlash('error', 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.');
+                return $this->render('security/reset_password.html.twig', ['token' => $token]);
+            }
+
+            $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
+            $user->setPassword($hashedPassword);
+            $user->setResetToken(null);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre mot de passe a été réinitialisé avec succès.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/reset_password.html.twig', ['token' => $token]);
+    }
+
+}
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,6 +10,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
 
 class RegistrationController extends AbstractController
 {
@@ -25,14 +30,31 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $plainPassword = $form->get('password')->getData();
-            $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
-            $user->setPassword($hashedPassword);
+            $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
+            if ($existingUser) {
+                $form->get('email')->addError(new FormError('Cette adresse e-mail est déjà utilisée.'));
+            } else {
+                $plainPassword = $form->get('password')->getData();
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
 
-            $entityManager->persist($user);
-            $entityManager->flush();
+                $entityManager->persist($user);
+                $entityManager->flush();
 
-            return $this->redirectToRoute('app_login');
+                $dsn = $_ENV['MAILER_DSN'];
+                $transport = Transport::fromDsn($dsn);
+                $mailer = new Mailer($transport);
+
+                $email = (new Email())
+                    ->from('florent.devynck@groupevitaminet.com')
+                    ->to($user->getEmail())
+                    ->subject('Confirmation de création de compte')
+                    ->text('Votre compte a bien été créé.');
+
+                $mailer->send($email);
+
+                return $this->redirectToRoute('app_login');
+            }
         }
 
         return $this->render('registration/register.html.twig', [
